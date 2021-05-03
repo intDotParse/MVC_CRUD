2bcde06841f471ee9ea65c2c466a2d5697fe6c49
Artemio Cabrillos Jr <mjbluesky08@gmail.com>
2021-05-03 19:35:44 +0800

    Add project files.

---
+++ MVC_CRUD/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_CRUD.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage ="Please enter the complete name!")]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+    }
+}
